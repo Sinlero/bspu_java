a1fba88448116a6fcf6d7f61236321d4393d1c32
goldfinch_p8a <wanted1345@gmail.com>
2020-07-23 13:19:40 +0900

    try and FileIput/OutputStream

--- src/Test/Main.java
+++ src/Test/Main.java
@@ -2,12 +2,6 @@ package Test;
 
 public class Main {
     public static void main(String[] args) {
-        Enum x = Enum.third;
 
-        if (x == Enum.second) {
-            System.out.println("true");
-        } else {
-            System.out.println("false");
-        }
     }
 }
