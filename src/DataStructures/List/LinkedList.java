7d371ee6e50f5b9a4b8b6554c946840bb6e28dba
goldfinch_p8a <wanted1345@gmail.com>
2020-07-30 13:19:49 +0900

    add methods: isEmpty, getItem, removeFirst work in progress: removeLast, checkOutOfBounds

--- src/DataStructures/List/LinkedList.java
+++ src/DataStructures/List/LinkedList.java
@@ -37,7 +37,7 @@ public class LinkedList implements Iterable {
     private int size = 0;
 
     public void addFirst(String value) {
-        if(first == null && last == null) {
+        if(this.isEmpty()) {
             first = last = new ListItem(value,null);
         } else {
             ListItem item = new ListItem(value, first);
@@ -47,7 +47,7 @@ public class LinkedList implements Iterable {
     }
 
     public void addLast(String value) {
-        if(first == null && last == null) {
+        if(this.isEmpty()) {
             first = last = new ListItem(value, null);
         } else {
             last.setNext(new ListItem(value,null));
@@ -56,27 +56,73 @@ public class LinkedList implements Iterable {
         size++;
     }
 
+    public String removeFirst() {
+        if (!this.isEmpty()) {
+            ListItem removable = first.getNext();
+            first = removable;
+            size--;
+            return removable.getValue();
+        } else {
+            return "";
+        }
+    }
+
+    @Deprecated
+    public String removeLast() {
+        if (!this.isEmpty()) {
+            ListItem current;
+            for (int i = 0; i < size - 1; i++) {
+//                current =
+            }
+            return "";
+        } else {
+            return "";
+        }
+    }
+
     public void showData() {
         ListItem item = first;
-        for (int i = 0; i < this.getSize(); i++) {
+        for (int i = 0; i < size; i++) {
             System.out.println(item.getValue());
             item = item.getNext();
         }
     }
 
     public String get(int index) {
-        if (index >= size) {
-            throw new IndexOutOfBoundsException();
+        try {
+            checkOutOfBounds(index);
+            ListItem item = first;
+            for (int i = 0; i < index; i++) {
+                item = item.getNext();
+            }
+            return item.getValue();
+        } catch (IndexOutOfBoundsException e) {
+            System.out.println("Выход за границы");
+            return "";
         }
-        ListItem item = first;
+    }
+
+    private ListItem getItem(int index) {
+        checkOutOfBounds(index);
+        ListItem current = first;
         for (int i = 0; i < index; i++) {
-            item = item.getNext();
+            current = current.getNext();
         }
-        return item.getValue();
+        return current;
+    }
+
+    @Deprecated
+    private void checkOutOfBounds(int index){
+        if (index >= size)
+            new IndexOutOfBoundsException();
     }
 
     public int getSize() {
         return this.size;
+    }
+
+    public boolean isEmpty() {
+        return (first == null && last == null);
     }
 
     @Override
--- src/DataStructures/Main.java
+++ src/DataStructures/Main.java
@@ -7,13 +7,19 @@ import java.util.ArrayList;
 public class Main {
     public static void main(String[] args) {
         LinkedList list = new LinkedList();
-        list.addFirst("hello");
-        list.addLast("world");
-        list.addFirst("Hey you!");
-        list.addLast("Motherfucker");
+//        list.addFirst("hello");
+//        list.addLast("world");
+//        list.addFirst("Hey you!");
+//        list.addLast("Motherfucker");
         list.showData();
-        System.out.println(list.get(3));
+        System.out.println();
+        list.removeFirst();
+        list.showData();
         System.out.println(list.getSize());
-        System.out.println(list.get(4));
+
+        System.out.println(list.isEmpty());
+
+        System.out.println(list.get(6));
+
     }
 }
