65540b7d640c995522a57cb72d4144959653dc2c
goldfinch_p8a <wanted1345@gmail.com>
2020-08-20 13:58:54 +0900

    init BinaryTree

---
+++ src/DataStructures/Tree/BinaryTree.java
@@ -0,0 +1,16 @@
+package DataStructures.Tree;
+
+public class BinaryTree {
+
+    private class Node<T> {
+        private T value;
+        private Node left;
+        private Node right;
+
+        public Node(Node left, T value, Node right) {
+            this.left = left;
+            this.value = value;
+            this.right = right;
+        }
+    }
+}
