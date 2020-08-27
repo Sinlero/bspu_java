2e055ed62754d331d6955f89a280315ef077c4c4
Goldfinch_p8a <wanted1345@gmail.com>
2020-08-27 13:34:06 +0900

    add getters and setters to Node

--- src/DataStructures/Tree/Node.java
+++ src/DataStructures/Tree/Node.java
@@ -10,4 +10,28 @@ public class Node<T> {
         this.value = value;
         this.right = right;
     }
+
+    public T getValue() {
+        return value;
+    }
+
+    public void setValue(T value) {
+        this.value = value;
+    }
+
+    public Node getLeft() {
+        return left;
+    }
+
+    public void setLeft(Node left) {
+        this.left = left;
+    }
+
+    public Node getRight() {
+        return right;
+    }
+
+    public void setRight(Node right) {
+        this.right = right;
+    }
 }
