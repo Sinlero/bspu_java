b22a274828561cf1deba14cd3f6d3cbfee261b72
Goldfinch_p8a <wanted1345@gmail.com>
2020-08-31 17:36:21 +0900

    example of work

---
+++ src/DataStructures/Tree/Main.java
@@ -0,0 +1,13 @@
+package DataStructures.Tree;
+
+public class Main {
+    public static void main(String[] args) {
+        BinaryTree tree = new BinaryTree(10);
+        tree.insert(9);
+        tree.insert(11);
+        tree.insert(15);
+        tree.insert(14);
+
+        System.out.println(tree.find(15));
+    }
+}
