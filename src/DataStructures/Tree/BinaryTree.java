905bfd18268f0e06e53b42cefdc225adf68efd79
Goldfinch_p8a <wanted1345@gmail.com>
2020-08-31 17:34:27 +0900

    add search node method, and add insert method

--- src/DataStructures/Tree/BinaryTree.java
+++ src/DataStructures/Tree/BinaryTree.java
@@ -1,5 +1,39 @@
 package DataStructures.Tree;
 
-public class BinaryTree<T> {
-    Node root;
+public class BinaryTree {
+    private Node root;
+    private Node tmp;
+
+    public BinaryTree(int value) {
+        root = new Node(null, value, null);
+    }
+
+    public Node find(int value) {
+        Node current = root;
+        while (current.getValue() != value) {
+            tmp = current;
+            if (value < current.getValue()) {
+                current = current.getLeft();
+            } else {
+                current = current.getRight();
+            }
+            if (current == null) {
+                return null;
+            }
+        }
+        return current;
+    }
+
+    public void insert(int value) {
+        Node current = find(value);
+        if (current == null) {
+            if (value > tmp.getValue()) {
+                tmp.setRight(new Node(null, value, null));
+            } else {
+                tmp.setLeft(new Node(null, value, null));
+            }
+        } else {
+            current.setRight(new Node(null, value, null));
+        }
+    }
 }
