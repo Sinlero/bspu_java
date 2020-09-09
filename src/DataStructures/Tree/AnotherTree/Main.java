565a27e81172d9618a1f56fcfeb15d0e652c8d2d
Sinlero <wanted1345@gmail.com>
2020-09-09 20:14:34 +0900

    example of work

---
+++ src/DataStructures/Tree/AnotherTree/Main.java
@@ -0,0 +1,14 @@
+package DataStructures.Tree.AnotherTree;
+
+public class Main {
+    public static void main(String[] args) {
+        Tree tree = new Tree(1000);
+        tree.insert(5);
+        tree.insert(20);
+        tree.insert(99);
+        tree.insert(45);
+        tree.insert(123);
+        tree.insert(13);
+        tree.insert(3);
+    }
+}
