78c34f7cdb7978589996d85c82df63354eeaab76
Sinlero <wanted1345@gmail.com>
2021-04-24 17:31:14 +0900

    complete encryption task

--- src/LabsOfMark/Lab3/Encryption.java
+++ src/LabsOfMark/Lab3/Encryption.java
@@ -1,4 +1,5 @@
 package LabsOfMark.Lab3;
+
 import java.util.*;
 
 public class Encryption {
@@ -6,15 +7,26 @@ public class Encryption {
     // ДЕЛАЕТ(6) ВОЗМОЖНЫМ(9) ВОЗНИКНОВЕНИЕ(13) УГРОЗЫ(6) = 103
     // key = ОТНОШЕНИЕ(9)
 
+    private static String alphabet = "АБВГДЕЖ3ИЙКЛМНОПРСТУФКЦЧШЩЬЫЪЭЮЯ";
+    private static String phrase = "УЯЗВИМОСТЬ КОМПЬЮТЕРНОЙ СИСТЕМЫ - ЭТО НЕКАЯ ЕЕ НЕУДАЧНАЯ ХАРАКТЕРИСТИКА, КОТОРАЯ " +
+            "ДЕЛАЕТ ВОЗМОЖНЫМ ВОЗНИКНОВЕНИЕ УГРОЗЫ";
+    private static String key = "ОТНОШЕНИЕ";  // (6,8,4,7,9,1,5,3,2)
+
     public static void main(String[] args) {
-        String alphabet = "АБВГДЕЖ3ИЙКЛМНОПРСТУФКЦЧШЩЬЫЪЭЮЯ";
-        String phrase = "УЯЗВИМОСТЬ КОМПЬЮТЕРНОЙ СИСТЕМЫ - ЭТО НЕКАЯ ЕЕ НЕУДАЧНАЯ " +
-                "ХАРАКТЕРИСТИКА, КОТОРАЯ ДЕЛАЕТ ВОЗМОЖНЫМ ВОЗНИКНОВЕНИЕ УГРОЗЫ";
-        String key = "ОТНОШЕНИЕ";  // (6,8,4,7,9,1,5,3,2)
-        System.out.println(phrase);
+        System.out.println("Ключ: " + key);
+        System.out.println("Исходный текст: " + phrase);
         phrase = cleanInputPhrase(phrase, key);
         List<List<String>> lists = createTable(phrase, key);
-        int[] keys = new int[key.length()];
+        int[] keys = getNums(key);
+        Map<Integer, List<String>> map = new TreeMap<>();
+        for (int i = 0; i < key.length(); i++) {
+            map.put(keys[i], lists.get(i));
+        }
+        System.out.println("Зашифрованный текст: " + getEncryptionFromMap(map));
+    }
+
+    public static int[] getNums(String key) {
+        int[] indexes = new int[key.length()];
         int index;
         int counter = 1;
         while (!key.trim().isEmpty()) {
@@ -22,25 +34,21 @@ public class Encryption {
                 if (key.contains(letter)) {
                     index = key.indexOf(letter);
                     key = key.replaceFirst(letter, " ");
-                    keys[index] = counter++;
+                    indexes[index] = counter++;
                     break;
                 }
             }
         }
-        Map<Integer, List<String>> map = new TreeMap<>();
-        for (int i = 0; i < key.length(); i++) {
-            map.put(keys[i], lists.get(i));
-        }
-        output(map);
+        return indexes;
     }
 
     public static String cleanInputPhrase(String phrase, String key) {
-        phrase = phrase.replaceAll(" ","")
-                .replaceAll(",","")
-                .replaceAll("-","")
+        phrase = phrase.replaceAll(" ", "")
+                .replaceAll(",", "")
+                .replaceAll("-", "")
                 .replaceAll("\\.", "")
                 .replaceAll("\\?", "")
-                .replaceAll("!","");
+                .replaceAll("!", "");
         int spaces = phrase.length() % key.length();
         for (int i = 0; i <= spaces; i++) {
             phrase += " ";
@@ -51,13 +59,12 @@ public class Encryption {
     public static List<List<String>> createTable(String phrase, String key) {
         int rows = phrase.length() / key.length();
         int columns = key.length();
-        String[][] table = new String[rows][columns];
         int counter = 0;
         List<List<String>> lists = new ArrayList<>();
         List<String> list;
-        for (int j = 0; j < columns; j++) {
+        for (int i = 0; i < columns; i++) {
             list = new ArrayList<>();
-            for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < rows; j++) {
                 list.add(String.valueOf(phrase.charAt(counter++)));
             }
             lists.add(list);
@@ -65,17 +72,27 @@ public class Encryption {
         return lists;
     }
 
-    public static void output(Map<Integer, List<String>> encrypted) {
+    public static String getEncryptionFromMap(Map<Integer, List<String>> encrypted) {
+        String[][] table = new String[encrypted.size()][encrypted.get(1).size()];
+        StringBuilder out = new StringBuilder();
         int counter = 0;
         for (List<String> value : encrypted.values()) {
-            for (String s : value) {
-                System.out.printf("%s", s);
-                counter++;
-                if (counter == 5) {
-                    System.out.print(" ");
-                    counter = 0;
-                }
+            String[] array = new String[encrypted.get(1).size()];
+            table[counter++] = value.toArray(array);
+        }
+        for (int j = 0; j < encrypted.get(1).size(); j++) {
+            for (int i = 0; i < encrypted.size(); i++) {
+                out.append(table[i][j]);
             }
         }
+        counter = 0;
+        for (int i = 0; i < out.length(); i++) {
+            counter++;
+            if (counter == 6) {
+                out.insert(i, " ");
+                counter = 0;
+            }
+        }
+        return out.toString();
     }
 }
